93897a6fa353df748e37275c54f284672d301e83
ElinAlice <nelializ02@gmail.com>
2017-10-03 16:22:09 -0500

    Encapsulamiento

---
+++ apppoo/PeronaNatural.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apppoo
+{
+    class PeronaNatural
+    {
+        private string Apellido;
+        private int Dni;
+        private string FechaNacimiento;
+        private string sexo;
+    }
+}
---
+++ apppoo/PersonaJuridica.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apppoo
+{
+    class PersonaJuridica
+    {
+        private string Ruc;
+        private string urlPaginaWeb;
+    }
+}
