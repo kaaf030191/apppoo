31f022ebcd768f45358a3514e348b15a95048b80
PepoBJ <bj112143@gmail.com>
2017-10-03 16:22:20 -0500

    Encapsulamiento [Persona|PersonaNatural|PersonaJuridica]

---
+++ apppoo/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apppoo
+{
+    class Persona
+    {
+        private string nombre;
+        private string dirección;
+        private string telefono;
+        private string correo_electronico;
+    }
+}
---
+++ apppoo/PersonaJuridica.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apppoo
+{
+    class PersonaJuridica
+    {
+        private string ruc;
+        private string url_pagina_web;
+    }
+}
