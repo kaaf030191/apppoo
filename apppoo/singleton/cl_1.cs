d291bb233e202617c497a52ec1ac873a873429d4
jkarlos1 <30414679+jkarlos1@users.noreply.github.com>
2017-10-04 17:25:14 -0500

    comit

---
+++ apppoo/singleton/cl_1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apppoo.NewFolder1
+{
+    class cl_1
+    {
+        private static cl_1 p;
+        public cl_1()
+        {
+            System.Windows.Forms.MessageBox.Show("Nuevo Singleton");
+        }
+        
+
+    public  static cl_1 Getinstace
+        {
+            get
+            {
+                if (p==null)
+                {
+                    p = new cl_1();
+                }
+                return p;
+            }
+
+
+        }
+
+        public string saludar(string nombre)
+        {
+            return "hola" + nombre;
+        }
+
+        string x = cl_1.Getinstace().saludar("svs");
+    }
+}
