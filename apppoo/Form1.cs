62ba94c99c07edb1ba3a011ff9aec87ca7a150fa
ElinAlice <nelializ02@gmail.com>
2017-10-04 16:35:00 -0500

    Una prueba de Singeton

--- apppoo/Form1.cs
+++ apppoo/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using apppoo.Singleton;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,12 @@ namespace apppoo
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            string xyz = SingleTom.getInstance.saludar("SVS");
+            MessageBox.Show(xyz);
         }
     }
 }
---
+++ apppoo/Singleton/SingleTom.cs
@@ -0,0 +1,25 @@
+﻿namespace apppoo.Singleton
+{
+    public class SingleTom
+    {
+        private static SingleTom s;
+        public SingleTom(){}
+
+        public static SingleTom getInstance
+        {
+            get{
+                if(s==null)
+                {
+                    s=new SingleTom();
+                }
+                return s;
+            }
+        }
+        public string saludar(string nombre)
+        {
+            return "hola"+nombre;
+
+        }
+       
+    }
+}
