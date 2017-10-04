30329381cd08bc734e64616b5835f4714fb2c619
jkarlos1 <30414679+jkarlos1@users.noreply.github.com>
2017-10-04 16:07:55 -0500

    singleton

---
+++ apppoo/singleton/cl_1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apppoo.NewFolder1
+{
+    class cl_1
+    {
+        private static cl_1 p;
+        public cl_1()
+        {
+
+        }
+        
+
+    public  static cl_1 Getinstace
+        {
+            get
+            {
+                if (p==null)
+                {
+                    p = new cl_1();
+                }
+                return p;
+            }
+
+
+        }
+
+        public string saludar(string nombre)
+        {
+            return "hola" + nombre;
+        }
+     }
+}
