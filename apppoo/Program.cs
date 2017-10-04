47d924b6915b2d206e21d833866399159e114293
PepoBJ <bj112143@gmail.com>
2017-10-04 16:34:35 -0500

    Prueba SingleTon

--- apppoo/Program.cs
+++ apppoo/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using apppoo.SingleTon;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace apppoo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmAgregarPersona());
+            Application.Run(new PruebaSingleTon());
         }
     }
 }
---
+++ apppoo/SingleTon/PruebaSingleTon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace apppoo.SingleTon
+{
+    public partial class PruebaSingleTon : Form
+    {
+        public PruebaSingleTon()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(SingleTon.getInstance.saludar("SVS"));
+        }
+    }
+}
