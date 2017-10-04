b48e772e96fe7e78c2949139870c31a1688ea7a9
Kevin Arnold Arias Figueroa <KAAF030191@gmail.com>
2017-10-04 17:40:09 -0500

    Patrón de diseño creacional Singleton.

---
+++ apppoo/Patron/Singleton.cs
@@ -0,0 +1,19 @@
+﻿public sealed class Singleton
+{
+    private static volatile Singleton instance;
+
+    private Singleton() { }
+
+    public static Singleton GetInstance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+
+            return instance;
+        }
+    }
+}
