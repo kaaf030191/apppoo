7cd1d8fa9b5ff5230369325795dd742422f89c48
danadomi <lopeznarvaez.65@gmail.com>
2017-10-04 22:39:19 -0500

    nueo Form1

--- apppoo/Program.cs
+++ apppoo/Program.cs
@@ -16,7 +16,7 @@ namespace apppoo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmPrincipal());
+            Application.Run(new Form1());
         }
     }
 }
