9beb14843d2e0f6889e2787a5932ef2dd257a3af
ElinAlice <nelializ02@gmail.com>
2017-10-09 17:56:30 -0500

    Pruebas de Test (Calculadora Test)

---
+++ AppCalculadora/Operacion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace AppCalculadora
+{
+    public class Operacion
+    {
+        public float Sumar(float numero1, float numero2)
+        {
+           // float resultado = numero1 + numero2;
+            // return resultado;
+            return numero1 + numero2;
+
+        }
+    }
+}
---
+++ AppCalculadoraTestx/OperacionText.cs
@@ -0,0 +1,16 @@
+﻿using AppCalculadora;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace AppCalculadoraTestx
+{
+    [TestClass]
+    public class OperacionText
+    {
+        [TestMethod]
+        public void SumarSieteYCuatroROnce()
+        {
+            Operacion operacion = new Operacion();
+            float resultado = operacion.Sumar(7, 4);
+            Assert.AreEqual(resultado,11);
+        }
+    }
+}
