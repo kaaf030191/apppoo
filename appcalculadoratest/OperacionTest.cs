fa282766d3bf5c112389039c18787a9e31ff9a03
juanyordyguillen <juanyordyguillen@gmail.com>
2017-10-09 18:06:57 -0500

    pruebas unitarias

--- appcalculadoratest/OperacionTest.cs
+++ appcalculadoratest/OperacionTest.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AppCalculadora;
+using NUnit.Framework;
 
 namespace appcalculadoratest
 {
-    class OperacionTest
+    [TestFixture]
+    public class OperacionTest
     {
+        Operacion operacion = null;
+        [SetUp]
+        public void Init()
+        {
+            operacion = new Operacion();
+        }
+
+        [TestCase]
+        public void SumarSieteYCuatroROnce()
+        {
+            float resultado = operacion.Sumar(7, 4);
+
+            Assert.That(resultado, Is.EqualTo(9)); 
+        }
+        [TestCase]
+        public void SumarSieteYSieteROnce()
+        {
+            float resultado = operacion.Sumar(7, 7);
+
+            Assert.That(resultado, Is.EqualTo(14));
+        }
+        [TearDown]
+        public void finish()
+        {
+            operacion = null;
+        }
+
     }
 }
