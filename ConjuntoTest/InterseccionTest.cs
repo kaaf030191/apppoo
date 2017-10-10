c207c4e08da1eb2798d309075d9850da6e380ca4
david226 <30418773+david226@users.noreply.github.com>
2017-10-10 13:00:31 -0500

    Interseccion

--- ConjuntoTest/InterseccionTest.cs
+++ ConjuntoTest/InterseccionTest.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using Conjuntos;
+using NUnit.Framework;
 namespace ConjuntoTest
 {
-    class InterseccionTest
+    [TestFixture]
+    public class InterseccionTest
     {
+        Interseccion interseccion = null;
+        [SetUp]
+        public void Init()
+        {
+            interseccion = new Interseccion();
+        }
+
+        [TestCase]
+        public void InterseccionArreglo()
+        {
+            int[] ArregloC = Interseccion.CalcularInterseccion(new[] { 2, 2, 1 }, new[] { 2, 3, 4 });
+            Assert.That(ArregloC, Is.EqualTo(new[] { 2 }));
+        }
+        [TearDown]
+        public void Finish()
+        {
+            interseccion = null;
+        }
     }
 }
--- Conjuntos/Interseccion.cs
+++ Conjuntos/Interseccion.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace Conjuntos
 {
-    class Interseccion
+    public class Interseccion
     {
+        public static int[] CalcularInterseccion(int[] arrayA, int[] arrayB)
+        {
+            int[] arrayC = new int[10];
+            int da = 0, cont = 0;
+            for (int i = 0; i < arrayA.Length; i++)
+            {
+                for (int j = 0; j < arrayB.Length; j++)
+                {
+                    if (arrayA[i] == arrayB[j])
+                    {
+                        arrayC[da] = arrayA[i];
+                        cont++;
+                        //return arrayC[da];
+                        Console.Write(arrayC[da] + "___");
+
+                    }
+
+
+                }
+            }
+            return arrayC;
+        }
     }
 }
