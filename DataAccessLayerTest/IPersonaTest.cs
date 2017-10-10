fd13ccbf06f0bdd78655606722b0b101cc592c1c
Kevin Arnold Arias Figueroa <KAAF030191@gmail.com>
2017-10-10 17:53:24 -0500

    Assert para un tipo Lista.

--- DataAccessLayerTest/IPersonaTest.cs
+++ DataAccessLayerTest/IPersonaTest.cs
@@ -15,27 +15,35 @@ namespace DataAccessLayerTest
         {
             operacion = new QPersona();
         }
+
         [TestCase]
         public void getByEdadVienteRCincoPersonas()
         {
             Mock<IPersona> moq = new Mock<IPersona>();
 
-            List<Persona> lista = new List<Persona>(new Persona[] {
-                    new Persona("Robert", "Huaman Caceres", "11111111", 20),
-                    new Persona("Juan", "Carrales Lopinta", "22222222", 20),
-                    new Persona("Karlos", "Rios Reyna", "33333333", 20)
-                });
-            moq.Setup(x => x.getByEdad("20")).Returns(lista);
+            moq.Setup(x => x.getByEdad("20")).Returns(new List<Persona>()
+            {
+                new Persona("Robert", "Huaman Caceres", "11111111", 20),
+                new Persona("Juan", "Carrales Lopinta", "22222222", 20),
+                new Persona("Karlos", "Rios Reyna", "33333333", 20)
+            });
 
             List<Persona> resultado = moq.Object.getByEdad("20");
 
+            List<Persona> listaPersonaTemp = new List<Persona>()
+            {
+                new Persona("Robert", "Huaman Caceres", "11111111", 20),
+                new Persona("Juan", "Carrales Lopinta", "22222222", 20),
+                new Persona("Karlos", "Rios Reyna", "33333333", 20)
+            };
 
-            Assert.That(resultado, Is.EquivalentTo(new List<Persona>(new Persona[] {
-                    new Persona("Robert", "Huaman Caceres", "11111111", 20),
-                    new Persona("Juan", "Carrales Lopinta", "22222222", 20),
-                    new Persona("Karlos", "Rios Reyna", "33333333", 20)
-                })));
+            for (int i = 0; i < resultado.Count; i++)
+            {
+                Assert.IsTrue(resultado[i].nombre.Equals(listaPersonaTemp[i].nombre));
+                Assert.IsTrue(resultado[i].apellido.Equals(listaPersonaTemp[i].apellido));
+            }
         }
+
         [TearDown]
         public void Finish()
         {
